56fc2f54d93ee937ede8f09e645af188ef39c88e
JoaquinSMV <joaquinity@gmail.com>
2025-10-06 12:01:11 +0200

    ejemplo 4

--- src/main/java/com/jsalmar/aad/AadApplication.java
+++ src/main/java/com/jsalmar/aad/AadApplication.java
@@ -1,35 +1,35 @@
 package com.jsalmar.aad;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @SpringBootApplication
-public class AadApplication {   // 👈 Declaración de clase
+public class AadApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AadApplication.class, args);
     }
 
-    public class FicheroTexto {
-        public static void main(String[] args) throws IOException {
-            Path ruta = Paths.get("alumnos.txt");
+    @Override
+    public void run(String... args) {
+        try (BufferedInputStream bis = new BufferedInputStream(
+                new FileInputStream("C:\\Users\\joaqu\\Documents\\Workspace\\AADD1\\src\\main\\resources\\Perro.jpg"))) {
 
-            // Escribir en el fichero
-            Files.writeString(ruta, "ID,Nombre\n1,Ana\n2,Juan", StandardCharsets.UTF_8);
+            byte[] buffer = new byte[1024];
+            int bytesLeidos;
+            int total = 0;
 
-            // Leer del fichero
-            String contenido = Files.readString(ruta, StandardCharsets.UTF_8);
-            System.out.println("Contenido del fichero:");
-            System.out.println(contenido);
+            while ((bytesLeidos = bis.read(buffer)) != -1) {
+                total += bytesLeidos;
+            }
+            System.out.println("Imagen leída con éxito. Total bytes: " + total);
+        } catch (IOException e) {
+            System.out.println("Error al leer el fichero: " + e.getMessage());
         }
     }
-
 }
-
-
